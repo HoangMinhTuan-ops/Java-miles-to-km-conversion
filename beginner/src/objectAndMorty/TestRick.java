6fe776a00d2ea733b7098000337d0bacd454d2e9
Accountant DE <Accountant DE@Lenovo-PC.localdomain>
2019-08-30 22:38:19 +0200

    Second commit
    upload ObjectAndMorty Package

---
+++ beginner/src/objectAndMorty/TestRick.java
@@ -0,0 +1,18 @@
+package objectAndMorty;
+
+public class TestRick {
+
+	public static void main(String[] args) {
+		
+		Rick rick1 = new Rick();
+		System.out.println("rick1 " + rick1.toString());
+		
+		Rick rick2 = new Rick(true, true, true);
+		System.out.println("rick2 " + rick2.toString());
+		
+		Rick rick3 = new Rick(false, false, false);
+		System.out.println("rick3 " + rick3.toString());
+		
+	}
+
+}
